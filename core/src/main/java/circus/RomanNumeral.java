15f4654f5278c9ecdfac4e6092b230f223989426
Miguel Eduardo Gil Biraud <miguel.gil.biraud@ieee.org>
2015-09-09 10:08:26 +0200

    Break all the things!

--- core/src/main/java/circus/RomanNumeral.java
+++ core/src/main/java/circus/RomanNumeral.java
@@ -18,7 +18,7 @@ public class RomanNumeral {
     private static int[]    numbers = { 1000,  900,  500,  400,  100,   90,
             50,   40,   10,    9,    5,    4,    1 };
 
-    private static String[] letters = { "M",  "CM",  "D",  "CD", "C",  "XC",
+    private static String[] letters = { "mil",  "CM",  "D",  "CD", "C",  "XC",
             "L",  "XL",  "X",  "IX", "V",  "IV", "I" };
 
     /**
